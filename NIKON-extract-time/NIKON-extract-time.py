edcd4dc8725e0e2820b0dc432f2f826f13a02ff6
jensloncke <jens.loncke@outlook.com>
2021-03-01 18:28:21 +0100

    added script to extract time from .nd2 file

---
+++ NIKON-extract-time/NIKON-extract-time.py
@@ -0,0 +1,29 @@
+import nd2reader
+import numpy as np
+from pathlib import Path
+from configuration.config import CONFIG
+
+
+if __name__ == "__main__":
+    import os
+
+    path_data = CONFIG["paths"]["data"]
+    path_time = CONFIG["paths"]["time"]
+    os.makedirs(CONFIG["paths"]["time"], exist_ok=True)
+
+    file_list = [filename for filename in os.listdir(path_data)
+                 if filename[-4:] == ".nd2" and os.path.isfile(path_data / filename)]
+    print(file_list)
+
+    for filename in file_list:
+        save_name = filename[:-4] + "_time.txt"
+        video = nd2reader.ND2Reader(path_data / filename)
+        time_information = video.timesteps / 1000
+        np.savetxt(path_time / save_name, time_information, fmt='%d')
+        # ca_bound = pd.read_excel(path_data / filename, sheet_name="340", engine='openpyxl')
+        # ca_bound.drop(ca_bound.columns[0], axis=1, inplace=True)
+        # ca_free = pd.read_excel(path_data / filename, sheet_name="380", engine='openpyxl')
+        # ca_free.drop(ca_free.columns[0], axis=1, inplace=True)
+        # df = ca_bound.div(ca_free)
+        # save_name = filename[:-5] + "_raw_ratio.html"
+        # plot_data(df, save_name, path_plots)
