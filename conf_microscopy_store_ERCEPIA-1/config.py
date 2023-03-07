99d3372da8d691fe468c776562fe56bfc22884ea
Jens Loncke <38840043+jensloncke@users.noreply.github.com>
2023-03-07 15:52:03 +0100

    Delete conf_microscopy_store_ERCEPIA-1 directory

--- conf_microscopy_store_ERCEPIA-1/config.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-CONFIG = {"path_analysis": Path("C:/Users/u0132307/Box Sync/PhD/Data/Ca2+_G-erCEPIA1_confocal/20201211-TrexERCEPIA.mdb"),
-          "path_response": Path("C:/Users/u0132307/Box Sync/PhD/Data/Ca2+_G-erCEPIA1_confocal/20201211-TrexERCEPIA.mdb/Response"),
-          "filename": "WT4-Results.csv",
-          "start_max": 4,
-          "stop_max": 125,
-          "start_min": 125,
-          "stop_min": 215}
